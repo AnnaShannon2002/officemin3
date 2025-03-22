b6f05e4e2bcdb3894c27e25a974287e5494d4eee
Anna Shannon (Student) <149623553+AnnaShannon2002@users.noreply.github.com>
2025-03-21 22:06:24 -0500

    Update authenticate.php for password hashing

--- authenticate.php
+++ authenticate.php
@@ -7,27 +7,33 @@ $username = "root";
 $password = "";
 $dbname = "officemin";
 
-$con = mysqli_connect($servername, $username, $password, $dbname);
-if (!$con) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-
 $uname = test_input($_POST['uname']);
 $pwd = test_input($_POST['pwd']);
 
 if (!empty($uname) && !empty($pwd)) {
-    $sql = "SELECT * FROM users WHERE username = '$uname' AND password = '$pwd'";
-    $result = mysqli_query($con, $sql);
-
-    if (mysqli_num_rows($result) > 0) {
-        $_SESSION['user'] = $uname;
-        mysqli_close($con);
-        header("location: index.php");
-        exit;
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+    
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
     }
-}
 
-mysqli_close($con);
+    $sql = "SELECT password FROM users WHERE username = ?";
+    $stmt = mysqli_prepare($conn, $sql);
+    mysqli_stmt_bind_param($stmt, "s", $uname);
+    mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt);
+
+    if ($row = mysqli_fetch_assoc($result)) {
+        if (password_verify($pwd, $row['password'])) {
+            $_SESSION['user'] = $uname;
+            mysqli_close($conn);
+            header("location: index.php");
+            exit;
+        }
+    }
+
+mysqli_close($conn);
+}
 ?>
 <!--<p>Basically, the code below displays to the user if the credentials are wrong.</p>-->
 <!DOCTYPE html>
